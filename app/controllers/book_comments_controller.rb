79d69302cc77c49cb81cab023110b98855e1807a
YamaShuhei <119114032+YamaShuhei@users.noreply.github.com>
2022-12-23 09:34:06 +0000

    save bookers2 add ajax/comments

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,12 +6,16 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
+
   end
 
   private
