45b784f69ce0a0f92c7d43a347a573da6d124963
YamaShuhei <119114032+YamaShuhei@users.noreply.github.com>
2022-12-24 20:59:39 +0000

    save bookerts2 add like ranking

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
@@ -8,6 +9,10 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
+
+  # def self.week_count
+  #   Book.joins(:favorites).where(favorites: { created_at: 0.days.ago.prev_week..0.days.ago.prev_week(:sunday)}).group(:id).order("count(*) desc")
+  # end
 
 def self.looks(search, word)
   if search == "perfect_match"
